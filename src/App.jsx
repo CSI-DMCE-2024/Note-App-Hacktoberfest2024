727cd656fe92b29c78fba7e21e80cc385cc2314d
Aditya Jambhale <jambhaleaditya91@gmail.com>
2024-10-03 20:59:25 +0530

    added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
-
+import Notfound from "./Components/NotFound"
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-
+          <Route path="/*" element={<Notfound />}></Route>
         </Routes>
       </Router>
     </>
